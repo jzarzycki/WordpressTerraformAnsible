eb13feab2c4b47e331b372518ea92cbc22ad3fef
Jan Zarzycki <jzarzycki97@gmail.com>
2023-12-05 14:42:29 +0100

    add private ssh key path to generated inventory

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 module "wordpress_prod" {
-  source              = "./modules/wordpress-hosting"
-  do_token            = var.do_token
-  ssh_public_key_path = var.ssh_public_key_path
+  source               = "./modules/wordpress-hosting"
+  do_token             = var.do_token
+  ssh_public_key_path  = var.ssh_public_key_path
+  ssh_private_key_path = var.ssh_private_key_path
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "ssh_public_key_path" {
   type        = string
   description = "Path to the public ssh key used to login to VPS"
 }
+
+variable "ssh_private_key_path" {
+  type        = string
+  description = "Path to the private ssh key used to login to VPS"
+}
